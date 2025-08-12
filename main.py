76ee01d8113ce6ed9382b2f6a029c4b27dee5b3a
Uni <chloe.bckr22@gmail.com>
2025-08-12 21:00:19 +0200

    fin de seance

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from movies_render import get_all_ghibli_movies, get_one_ghibli_movies
 from requests.exceptions import HTTPError
-from Httpexception import NotFoundError, InternalServerError
+from httpexception import NotFoundError, InternalServerError
 
 # call the api
 app = FastAPI()
@@ -14,11 +14,15 @@ def show_all_movies(request: Request):
     try :
         return get_all_ghibli_movies(request)
     except HTTPError as e:
-        if e.response.status_code == 404:
-            raise NotFoundError("Pokemon not found")
         raise InternalServerError(str(e))        
 
 # route to get one movies
 @app.get("/api/ghibli/films/{id_movie}")
 def show_one_movie(request: Request, id_movie: str):
-    return get_one_ghibli_movies(request, id_movie)
+    try:
+        return get_one_ghibli_movies(request, id_movie)
+    except HTTPError as e:
+        if e.response.status_code == 404:
+            raise NotFoundError(f"Movie with id {id_movie} not found.")
+        else:
+            raise InternalServerError(str(e))
